29162979d9b9c9cf70fc593fa5494f4fe21f1bdd
Ajahid <97071675+Ajahid@users.noreply.github.com>
2022-01-04 16:52:47 -0800

    Update Git1.java
    This is for practice

--- src/Git1.java
+++ src/Git1.java
@@ -3,13 +3,19 @@ public class Git1 {
 
 	public static void main(String[] args) {
 		
-
+		System.out.println("This is my my first git hub account");
+		
 	}
 
 	public void methodA (){
 	
 		System.out.println ("This is chnages from upstream, upstream means in remote repository ");
 		
-	}		
+	}
+	public void methodB (){
+		
+		// This is second method for practice purpose
+		
+	}
 	
 }
